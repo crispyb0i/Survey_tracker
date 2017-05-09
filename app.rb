6f1677a2faf4d56e865044a53fc13cbfce11d375
Dan Jacob and David Shin <dan+david@>
2017-05-09 16:40:15 -0700

    add stuff

--- app.rb
+++ app.rb
@@ -45,19 +45,21 @@ end
 
 get('/survey/:id') do
   @questions = Question.all()
-  @survey = Survey.find(params.fetch('id'))
+  @survey = Survey.find(params.fetch('id').to_i)
   erb(:survey)
 end
 
 post('/survey/:id/question') do
   @questions = Question.all()
   @survey = Survey.find(params.fetch('id'))
-  question = params.fetch('question')
-  Question.new(:content => question, :survey_id => @survey.id).save
+  questions.save()
   erb(:survey)
 end
 
 post('/survey/:id/question/edit') do
-  @question = Question.find(params.fetch('id'))
+  question_input = params.fetch('question')
+  @survey = params.fetch('id')
+  Question.new(:content => question_input, :survey_id => @survey).save
+  @question = Question.find(params.fetch('id').to_i)
   erb(:question_edit)
 end
